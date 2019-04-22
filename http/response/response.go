ec218a6cc8e2f2ecd0010f06f8fdf8921b9b21e8
David Pinheiro <davepinh@gmail.com>
2019-04-22 15:12:47 -0300

    Response header data structure created

---
+++ http/response/response.go
@@ -0,0 +1,46 @@
+package response
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dapine/saws/fs"
+	"github.com/dapine/saws/http/request"
+	"github.com/dapine/saws/http/resource"
+)
+
+// TODO: Finish this
+var StatusName map[int]string = map[int]string{
+	200: "OK",
+	404: "Not Found",
+}
+
+type Header struct {
+	// TODO: Implement more fields
+	httpVersion string
+	statusCode  int
+	statusName  string
+	date        time.Time
+	resource    resource.Resource
+}
+
+func NewHeader(httpVersion string, req request.Request) Header {
+	t := time.Now().UTC()
+	r, err := fs.ReadResource(req.RequestLine().RequestUri())
+	if err != nil {
+		sc := 404
+		return Header{httpVersion: httpVersion, statusCode: sc, statusName: StatusName[sc], date: t, resource: resource.Empty}
+	}
+
+	sc := 200
+
+	return Header{httpVersion: httpVersion, statusCode: sc, statusName: StatusName[sc], date: t, resource: r}
+}
+
+func (h Header) String() string {
+	d := h.date.Format(time.RFC1123)
+	lm := h.resource.LastModified().UTC().Format(time.RFC1123)
+	return fmt.Sprintf("%s %d %s\r\nDate: %s\r\nLast-Modified: %s\r\nContent-Length: %d\r\nContent-Type: %s\r\n\r\n%s", h.httpVersion, h.statusCode,
+		h.statusName, d, lm, h.resource.Length(), h.resource.ContentType(),
+		h.resource.Data())
+}
--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"net"
-	"time"
 
-	"github.com/dapine/saws/fs"
 	"github.com/dapine/saws/http/request"
+	"github.com/dapine/saws/http/response"
+)
+
+const (
+	httpVersion = "HTTP/1.1"
 )
 
 func main() {
@@ -28,21 +30,9 @@ func main() {
 }
 
 func sendResponse(req request.Request) {
-	status := "HTTP/1.1 200 OK"
-	t := time.Now().UTC()
-	date := t.Format(time.RFC1123)
-	resource, err := fs.ReadResource(req.RequestLine().RequestUri())
-	if err != nil {
-		log.Println("Resource not found:", err)
-	}
-	ct := resource.ContentType()
-	cl := resource.Length()
-	lm := resource.LastModified().UTC().Format(time.RFC1123)
-	content := resource.Data()
+	header := response.NewHeader(httpVersion, req)
 
-	getResponseStr := fmt.Sprintf("%s\r\nDate: %s\r\nLast-Modified: %s\r\nContent-Length: %d\r\nContent-Type: %s\r\n\r\n%s", status, date, lm, cl, ct, string(content[:]))
-
-	_, err = req.Connection().Write([]byte(getResponseStr))
+	_, err := req.Connection().Write([]byte(header.String()))
 	if err != nil {
 		log.Println("Could not response: ", err)
 	}
